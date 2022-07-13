be09b54ac54f760a0f7cfc46baa4b7131811ab8a
James Hannan <jim9112@gmail.com>
2022-07-13 16:48:49 -0400

    add login function

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import useUpdateForm from '../hooks/useUpdateForm';
+import { auth } from '../firebaseIndex';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 const LoginForm = () => {
   const [updateForm, formData] = useUpdateForm();
   const handleForm = (e: React.FormEvent) => {
     e.preventDefault();
+    signInWithEmailAndPassword(auth, formData.email, formData.password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
     console.log(formData);
   };
   return (
