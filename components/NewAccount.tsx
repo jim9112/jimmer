da447bee7829eda883c00f66cf2f4a1325d1a0eb
James Hannan <jim9112@gmail.com>
2022-07-19 17:26:56 -0400

    add display modes

---
+++ components/NewAccount.tsx
@@ -0,0 +1,9 @@
+const NewAccount = () => {
+  return (
+    <div>
+      <h2>New Account</h2>
+    </div>
+  );
+};
+
+export default NewAccount;
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,12 +1,19 @@
 import LoginForm from '../components/LoginForm';
 import SignupForm from '../components/SignupForm';
+import NewAccount from '../components/NewAccount';
+import { useState } from 'react';
 
 const Login = () => {
+  const [displayMode, setDisplayMode] = useState<
+    'signUp' | 'signIn' | 'accountDetails'
+  >('signUp');
+
   return (
     <main>
       <h1>Happening now</h1>
-      <SignupForm />
-      <LoginForm />
+      {displayMode === 'signUp' && <SignupForm />}
+      {displayMode === 'signIn' && <LoginForm />}
+      {displayMode === 'accountDetails' && <NewAccount />}
     </main>
   );
 };
